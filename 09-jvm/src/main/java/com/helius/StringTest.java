ae15f3918820e8f4533c46e521cc56639ac74aae
chengfei.jin@infaith.com.cn <12345678>
2020-05-03 21:07:06 +0800

    日常更新，代码记录尔

--- 09-jvm/src/main/java/com/helius/StringTest.java
+++ 09-jvm/src/main/java/com/helius/StringTest.java
@@ -5,4 +5,10 @@ package com.helius;
  * @Create 2020-05-02-4:16 下午
  */
 public class StringTest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("c = " + c);
+    }
 }
